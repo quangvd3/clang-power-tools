4424605356434e66b9cfc45520d84c0de69cbd9f
Enache Ionut <ionut.enache@caphyon.com>
2019-10-07 13:45:53 +0300

    show login view if the user token expired

--- ClangPowerTools/ClangPowerTools/Commands/CommandController.cs
+++ ClangPowerTools/ClangPowerTools/Commands/CommandController.cs
@@ -13,6 +13,7 @@ using System.IO;
 using System.Linq;
 using Task = System.Threading.Tasks.Task;
 using ClangPowerTools.MVVM.Controllers;
+using System.Net.Http;
 
 namespace ClangPowerTools
 {
@@ -106,19 +107,33 @@ namespace ClangPowerTools
 
     public async void Execute(object sender, EventArgs e)
     {
-      FreeTrialController freeTrialController = new FreeTrialController();
-
-      if (freeTrialController.WasEverInTrial() == false && activeLicense == false)
+      var freeTrialController = new FreeTrialController();
+      var licenseController = new LicenseController();
+      
+      bool activeAccount = (await licenseController.CheckUserTokenHttpResultAsync(new TokenModel())).IsSuccessStatusCode;
+      bool tokenExists = licenseController.TokenExists(new TokenModel(), out TokenModel newToken);
+      
+      // First app install - choose license
+      if (freeTrialController.WasEverInTrial() == false && activeAccount == false)
       {
         LicenseView licenseView = new LicenseView();
         licenseView.ShowDialog();
         return;
       }
 
-      if (freeTrialController.IsActive() == false && activeLicense == false)
+      // Trial expired
+      if (freeTrialController.IsActive() == false && activeAccount == false && tokenExists == false)
       {
         TrialExpiredView trialExpiredView = new TrialExpiredView();
         trialExpiredView.ShowDialog();
+        return;
+      }
+
+      // Session Expired
+      if (freeTrialController.IsActive() == false && activeAccount == false && tokenExists)
+      {
+        LoginView loginView = new LoginView();
+        loginView.ShowDialog();
         return;
       }
 
